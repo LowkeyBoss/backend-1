4c6774b7440fa9cffdc04419029f40c973142936
LowkeyBoss <161791760+LowkeyBoss@users.noreply.github.com>
2025-07-22 16:20:42 -0700

    Update env.d.ts

--- env.d.ts
+++ env.d.ts
@@ -2,9 +2,9 @@ declare global {
   namespace NodeJS {
     interface ProcessEnv {
       DATABASE_URL: string;
-      BOT_TOKEN: string;
-      BOT_CLIENT_ID: string;
-      BOT_CLIENT_SECRET: string;
+      BOT_TOKEN: MTM5Mzc1MDk3Mjc5MDA4MzYxNA.Gcx9fu.8AzSM-3kNEdzvObTU9ML-R55OvoAESUj7y_EwU;
+      BOT_CLIENT_ID: 1393750972790083614;
+      BOT_CLIENT_SECRET: j3P8bPUi6fSpVBHsj1Jd7iEjmA4cdZC6;
 
       /**
        * The url where the frontend is hosted
@@ -18,7 +18,7 @@ declare global {
        *
        * ex: `https://my-bot.up.railway.app`, default: `http://localhost:8080`
        */
-      STATIC_URL?: string;
+      STATIC_URL?: modern-dashboard-two.vercel.app;
     }
   }
 }
